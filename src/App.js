1a9fd2c30f32707bce898ffd5b77a8dfcc1204f7
Fady-Samy <fady.samy1994@yahoo.com>
2021-05-14 22:03:30 +0200

    Changes to be committed (removed all unnecessary files): 	modified:   .gitignore 	modified:   package-lock.json 	modified:   src/App.js 	deleted:    src/CreateContact.js 	deleted:    src/ImageInput.js 	deleted:    src/ListContacts.js 	deleted:    src/icons/arrow-back.svg 	deleted:    src/icons/cancel.svg 	deleted:    src/icons/person-add.svg 	deleted:    src/icons/person.svg 	deleted:    src/icons/search.svg 	modified:   src/index.css 	deleted:    src/utils/ContactsAPI.js 	modified:   yarn.lock
    Added the solution to app.js and working as requested in exercise 1
    
    Added the instructions file of the exercise

--- src/App.js
+++ src/App.js
@@ -1,58 +1,114 @@
-import React, { Component } from 'react'
-import ListContacts from './ListContacts'
-import * as ContactsAPI from './utils/ContactsAPI'
-import CreateContact from './CreateContact'
-import { Route } from 'react-router-dom'
+import React, { Component } from 'react';
+
+/*
+Use React and the data below to display a list of users alongside their favorite movies.
+
+For detailed instructions, refer to instructions.md.
+*/
+
+const profiles = [
+  {
+    id: 1,
+    userID: '1',
+    favoriteMovieID: '1',
+  },
+  {
+    id: 2,
+    userID: '2',
+    favoriteMovieID: '1',
+  },
+  {
+    id: 3,
+    userID: '4',
+    favoriteMovieID: '5',
+  },
+  {
+    id: 4,
+    userID: '5',
+    favoriteMovieID: '2',
+  },
+  {
+    id: 5,
+    userID: '3',
+    favoriteMovieID: '5',
+  },
+  {
+    id: 6,
+    userID: '6',
+    favoriteMovieID: '4',
+  },
+];
+
+const users = {
+  1: {
+    id: 1,
+    name: 'Jane Cruz',
+    userName: 'coder',
+  },
+  2: {
+    id: 2,
+    name: 'Matthew Johnson',
+    userName: 'mpage',
+  },
+  3: {
+    id: 3,
+    name: 'Autumn Green',
+    userName: 'user123',
+  },
+  4: {
+    id: 4,
+    name: 'John Doe',
+    userName: 'user123',
+  },
+  5: {
+    id: 5,
+    name: 'Lauren Carlson',
+    userName: 'user123',
+  },
+  6: {
+    id: 6,
+    name: 'Nicholas Lain',
+    userName: 'user123',
+  },
+};
+
+const movies = {
+  1: {
+    id: 1,
+    name: 'Planet Earth 1',
+  },
+  2: {
+    id: 2,
+    name: 'Selma',
+  },
+  3: {
+    id: 3,
+    name: 'Million Dollar Baby',
+  },
+  4: {
+    id: 4,
+    name: 'Forrest Gump',
+  },
+  5: {
+    id: 5,
+    name: 'Get Out',
+  },
+};
 
 class App extends Component {
-  state = {
-    contacts: []
-  }
-  componentDidMount() {
-    ContactsAPI.getAll()
-      .then((contacts) => {
-        this.setState(() => ({
-          contacts
-        }))
-      })
-  }
-  removeContact = (contact) => {
-    this.setState((currentState) => ({
-      contacts: currentState.contacts.filter((c) => {
-        return c.id !== contact.id
-      })
-    }))
-
-    ContactsAPI.remove(contact)
-  }
-  createContact = (contact) => {
-    ContactsAPI.create(contact)
-      .then((contact) => {
-        this.setState((currentState) => ({
-          contacts: currentState.contacts.concat([contact])
-        }))
-      })
-  }
   render() {
     return (
       <div>
-        <Route exact path='/' render={() => (
-          <ListContacts
-            contacts={this.state.contacts}
-            onDeleteContact={this.removeContact}
-          />
-        )} />
-        <Route path='/create' render={({ history }) => (
-          <CreateContact
-            onCreateContact={(contact) => {
-              this.createContact(contact)
-              history.push('/')
-            }}
-          />
-        )} />
+      	{profiles.map(profile=>{
+         	let user = users[profile.userID].name;
+    	 	let favMovie= movies[profile.favoriteMovieID].name;
+    		return(
+        		<p key={profile.id}> {user} Favorite Movie is {favMovie}  </p>
+        	)
+      	})}
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
